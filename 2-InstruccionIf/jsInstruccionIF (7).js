a9df40fd7a116b9e09620f29d10ea50cb65d306e
Jigen21 <axel.quinteros@davinci.edu.ar>
2017-02-10 16:55:47 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -3,10 +3,12 @@ function Mostrar()
 //tomo la edad  
   var Edad;
   var Estado;
+  var Soltero;
   Edad=document.getElementById('edad').value;
   Estado=document.getElementById('estadoCivil').value;
-  alert(Estado);
-  if(Edad<18)
+  Soltero="Soltero"
+
+  if(Edad<18&&Estado!=Soltero)
   {
   	
   		alert("es muy pequeño para NO ser soltero");
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -3,12 +3,17 @@ function Mostrar()
 //tomo la edad  
 var Edad;
 var Estado;
-Edad=document.getElementById('edad').value
-Estado=document.getElementById('estadoCivil')
-Edad=parseInt(Edad);
-if()
+var Soltero;
+Edad=document.getElementById('edad').value;
+Estado=document.getElementById('estadoCivil').value;
+Soltero="Soltero"
+if(Edad<18&&Estado!=Soltero)
 {
-
+	
+}
+else(Edad>=18)
+{
+	alert("es Soltero y no es menor")
 }
 
 	
