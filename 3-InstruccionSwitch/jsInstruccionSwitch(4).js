873d2f622350f6dfc948f10c123553ea1ebe7195
Jigen21 <axel.quinteros@davinci.edu.ar>
2017-02-10 22:05:41 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,8 +4,27 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+// febrero 28  abril 30  junio 30   septiembre 30  noviembre 30 
 	
-	
+	switch(mesDelAño)
+	{
+
+		case"Abril":
+		case"Junio":
+		case"Septiembre":
+		case"Noviembre":
+		alert("Tienen 30 dias");
+		break;
+
+		case"Febrero":
+		alert("Tiene 28 dias");
+		break;
+
+		default:
+		alert("Tienen 31 dias");
+		break;
+
+	}
 
 
 
