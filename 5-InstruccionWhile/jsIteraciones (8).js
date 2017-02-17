0f696f8752243a3beee8e7bcf1459c8d3d411d50
Jigen21 <axel.quinteros@davinci.edu.ar>
2017-02-17 14:18:25 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,25 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero;
 	
 	var respuesta='si';
+
+	while(respuesta=="si")
+	{
+		contador++;
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+		respuesta=prompt("ingrese Si para continuar");
+		if(numero>0)
+		{
+		positivo=positivo+numero;
+		}
+		if(numero<0)
+		{
+		   negativo=negativo*numero
+		}
+	}
 
 
 document.getElementById('suma').value=positivo;
