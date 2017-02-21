4d13abf78fc6e85b053dc23c5a782c096546b8e8
Jigen21 <axel.quinteros@davinci.edu.ar>
2017-02-21 15:27:05 -0300

    Add files via upload

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -7,7 +7,94 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
+ 	var NumeroIngresado;
+ 	var Eleccion;
+ 	var NumeroDivisor=1;
+    
+
 function ComenzarIngreso () 
 {
- 	
+    
+
 }
+
+
+
+function NumerosPares()
+{
+   NumeroIngresado=document.getElementById('numero').value;
+   NumeroIngresado=parseInt(NumeroIngresado);
+   while(NumeroIngresado>0)
+   {
+
+   	if(NumeroIngresado % 2 == 0)
+   	{
+       alert(NumeroIngresado);
+   	}
+   	NumeroIngresado=NumeroIngresado-1;
+
+   }
+   
+   
+}
+
+function NumerosImpares()
+{
+   NumeroIngresado=document.getElementById('numero').value;
+   NumeroIngresado=parseInt(NumeroIngresado);
+   while(NumeroIngresado>0)
+   {
+
+   	if(NumeroIngresado % 2 != 0)
+   	{
+       alert(NumeroIngresado);
+   	}
+   	NumeroIngresado=NumeroIngresado-1;
+
+   }
+   
+   
+}
+
+function NumerosDivisibles()
+{
+   NumeroIngresado=document.getElementById('numero').value;
+   NumeroIngresado=parseInt(NumeroIngresado);
+   NumeroDivisor=parseInt(NumeroDivisor);
+   
+   while(NumeroDivisor<101)
+   {
+   	 if(NumeroIngresado%NumeroDivisor == 0)
+   	 {
+   	 	alert(NumeroDivisor);
+   	 }
+   	 NumeroDivisor++;
+   }
+}
+
+function VerificarPrimo()   //Le falta a este
+{
+   NumeroIngresado=document.getElementById('numero').value;
+   NumeroIngresado=parseInt(NumeroIngresado);
+   NumeroDivisor=NumeroIngresado;
+
+   while(NumeroIngresado>0)
+   {
+   	 NumeroIngresado--;
+   	 if(NumeroIngresado%NumeroDivisor == 0)
+   	 {
+   	 	alert(NumeroIngresado)
+   	 }
+   	 
+   }
+  
+}
+
+function NumerosPrimos()
+{
+   alert("asdasd5");
+}
+
+
+
+
