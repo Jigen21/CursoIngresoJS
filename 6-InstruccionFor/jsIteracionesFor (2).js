f1654b1895db29a9049a279f018e84294a7a8f72
Jigen21 <axel.quinteros@davinci.edu.ar>
2017-02-24 19:35:15 -0300

    2 del for

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -14,10 +14,15 @@ function Mostrar()
    var edadmayor=0;
    var edadmaxima=0;
    var edadminima=0;
+   var sexo;
+   var mujer=0;
+   var EdadDelGato;
+   var NombreDelPrimerGato;
+   var PrimerGato="False";
 
-   for(cont=0;cont<3;cont++)
+   for(cont=0;cont<2;cont++)
   {
-  	 mascota=prompt("Tienen mascota?")
+  	 mascota=prompt("Tienen mascota?");
 
      while(mascota!="si"&&mascota!="no")
      {
@@ -25,37 +30,74 @@ function Mostrar()
      }
 
     
-     if(mascota=="si")
+     if(mascota=="si")//Cuando si tiene mascota
      {
        mascotasi++;
-       alert("Tienen mascota "+mascotasi);
+       
+       sexo=prompt("Es Mujer u Hombre");//Pregunto el genero
+       while(sexo!="m"&&sexo!="h")
+        {
+          sexo=prompt("Solo escriba m o h");
+        }
 
-       mascota=prompt("Es Perro o Gato?")
+        edad=prompt("Ingrese su edad");//Pregunto la edad al dueño
+
+
+       mascota=prompt("Es Perro o Gato?")//pregunto si es perro o gato
        while(mascota!="perro"&&mascota!="gato")
-       {
-       	mascota=prompt("Ingresa Perro o Gato")
-       }
-       if(mascota=="perro")
-       {
-       	perro++;
-       	alert(perro);
-       }
-       if(mascota=="gato")
-       {
-       	gato++;
-       	alert(gato);
-       }
+        {
+       	  mascota=prompt("Ingresa Perro o Gato");
+        }
 
-       //Necesito la persona mas vieja que no tenga mascota
+       if(mascota=="perro")//Si es perro
+        {
+       	  perro++;
+       	  
+        }
+
+       if(mascota=="gato")//Si es gato
+        {
+       	  gato++;
+        	
+
+          NombreDelGato=prompt("Ingrese el nombre de su gato");//Pregunto el nombre del gato
+
+          EdadDelGato=prompt("Ingrese la edad de su gato");//Pregunto la edad del gato
+
+
+           while(EdadDelGato<0||EdadDelGato>15)
+           {
+              EdadDelGato=prompt("Por favor,ingrese una edad valida de 0 a 15");
+
+           }
+
+
+           if(PrimerGato=="False"&&sexo=="m"&&edad>30&&EdadDelGato<=5)//Pregunto si es el primero
+               {
+                 NombreDelPrimerGato=NombreDelGato;
+                 PrimerGato="True";
+                 
+
+               }
+
+
+        }
+
+
+     
+
+        
+
+       //Necesito la persona mas vieja que no tenga mascota(listo)
        //Tarea,Necesito saber el nombre del primer gato que sea de una mujer mayor a 30 años y el gato no tenga mas de 5 años de edad(validacion de 0 a 15 edad del gato)
      }
 
-     if(mascota=="no")
+     if(mascota=="no")  //Cuando No tiene mascota
        {
  	   mascotano++;
  	   edad=prompt("Ingrese su edad")
 
- 	   if(edad>edadmaxima)
+ 	   if(edad>edadmaxima) //Para saber la edad mas vieja que no tenga mascota
 	  	{
           edadmaxima=edad;
           edadmayor=edad;
@@ -68,5 +110,6 @@ function Mostrar()
        }
   }
   alert(edadmayor);
+  alert(NombreDelPrimerGato);
 
 }
